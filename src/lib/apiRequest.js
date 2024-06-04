0878958db0506e4b647354b8b1cdc3608173290e
BuckanianMark <kariukimark115@gmail.com>
2024-06-04 20:58:42 +0300

    Updating server url

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:8080/api",
+    baseURL:"https://real-estate-prisma-api.onrender.com",
+    //baseURL:"http://localhost:8080/api",
     withCredentials:true
 })
 export default apiRequest
