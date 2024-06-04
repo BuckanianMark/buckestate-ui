761641e0cdfbba1ed58a28ac9ce1556eec14fdf1
BuckanianMark <kariukimark115@gmail.com>
2024-06-04 10:02:08 +0300

    handling the navbar issue

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,21 @@
 import "./navbar.scss"
 import Logo from "../../assets/logo.png"
 import Menu from "../../assets/menu.png"
+import Close from "../../assets/close.png"
 import Avatar from "../../assets/avatar.png"
 import { useContext, useState } from "react"
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
-function Navbar(){
-    
+function Navbar(){   
     const {currentUser} = useContext(AuthContext);
     const [open,setOpen] = useState(false);
-    
+    const handleOpen =() => {
+        setOpen(true)
+    }
+    const handleClose =() => {
+        setOpen(false)
+    }
 return(
     <nav>
         <div className="left">
@@ -40,15 +45,24 @@ return(
             <a href="/register" className="register">Sign Up</a></>)}
            
             <div className="menuIcon">
-                <img src={Menu} alt="" width="50" onClick={() => setOpen(!open)} />
+                {open ? <><img src={Close} alt="" width="50" onClick={handleClose} /></> 
+                :
+                <><img src={Menu} alt="" width="50" onClick={handleOpen} /></>}
+                
             </div>
-            <div className={open ? "menu active" : "menu"}>
-            <a href="">Home</a>
-            <a href="">About</a>
-            <a href="">Contact</a>
-            <a href="">Agents</a>
-            <a href="/login">Sign In</a>
-            </div>
+            {open && (
+             <div
+             data-aos="fade-right"
+             data-aos-once="true"
+              className={open ? "menu active" : "menu"}>
+             <a href="">Home</a>
+             <a href="">About</a>
+             <a href="">Contact</a>
+             <a href="">Agents</a>
+             <a href="/login">Sign In</a>
+             </div>
+            )}
+           
         </div>
     </nav>
 )
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import App from './App.jsx'
 import './index.scss'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 import { SocketContextProvider } from './context/SocketContext.jsx'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init({
+  animatedClassName: 'aos-animate',
+  delay: 0,
+  duration: 800, 
+  easing: 'ease',
+  mirror: false,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
