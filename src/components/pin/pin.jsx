b87044c696361397cf17cd98e2c1773e2fd397c1
BuckanianMark <kariukimark115@gmail.com>
2024-06-16 16:11:46 +0300

    Configuring the Marker

--- src/components/pin/pin.jsx
+++ src/components/pin/pin.jsx
@@ -1,11 +1,22 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom'
+import MarkerIcon from '../../assets/marker.png'
 import './pin.scss'
 import {Marker,Popup} from 'react-leaflet'
+import L from 'leaflet';
 function Pin({item}){
+
+    const markerIcon = new L.Icon({
+        iconUrl: {MarkerIcon},
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34]
+      });
+      
+
     // console.log(item)
     return(
-        <Marker position={[item.latitude,item.longitude]}>
+        <Marker position={[item.latitude,item.longitude]} icon={markerIcon}>
         <Popup>
          <div className="popupContainer">
             <img src={item.images[0]} alt="rental-image" />
