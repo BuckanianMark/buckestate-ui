5355a6117554b0207cba5c9077d44e017c392a9d
BuckanianMark <kariukimark115@gmail.com>
2024-06-16 14:46:47 +0300

    Fixin the map section

--- src/components/pin/pin.jsx
+++ src/components/pin/pin.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 import './pin.scss'
 import {Marker,Popup} from 'react-leaflet'
 function Pin({item}){
+    // console.log(item)
     return(
         <Marker position={[item.latitude,item.longitude]}>
         <Popup>
          <div className="popupContainer">
-            <img src={item.img} alt="rental-image" />
+            <img src={item.images[0]} alt="rental-image" />
             <div className="textContainer">
                 <Link to={`/${item.id}`} >{item.title}</Link>
                 <span className='bed'> {item.bedroom} bedroom </span>
